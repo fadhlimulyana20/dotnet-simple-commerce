da33624519d845770b67557d4ca3a432cb29223d
Fadhli Mulyana <baba.fadhli@gmail.com>
2024-10-13 15:14:18 +0700

    feat: add login & register page

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_mvc.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
--- obj/Debug/net8.0/dotnet-mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6301584d64ac3b7bc559d4e540d34339a3cc6fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cc4425267c0a19d47412aca6acd537df7706ac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
